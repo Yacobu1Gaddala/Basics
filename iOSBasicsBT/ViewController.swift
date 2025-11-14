d51c4d4236b3d226df7697c5c689281334d39a78
Gaddala Yacobu <126049931+Yacobujet2022@users.noreply.github.com>
2025-11-14 18:26:52 +0530

    Title added

--- iOSBasicsBT/ViewController.swift
+++ iOSBasicsBT/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .blue
+       let label = UILabel()
+        label.frame = CGRect(x: 20, y: 50, width: 200, height: 50)
+        label.text = "Github is awesome"
+        self.view.addSubview(label)
+        
+        
         
         
     }
