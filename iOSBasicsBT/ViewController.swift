e33cf44b1edc9c9ab842740f02c2d0b2f28f0404
Gaddala Yacobu <126049931+Yacobujet2022@users.noreply.github.com>
2025-11-14 18:30:58 +0530

    changed

--- iOSBasicsBT/ViewController.swift
+++ iOSBasicsBT/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
        let label = UILabel()
         label.frame = CGRect(x: 20, y: 50, width: 200, height: 50)
-        label.text = "Github is awesome"
+        label.text = "Github"
         self.view.addSubview(label)
         
         
